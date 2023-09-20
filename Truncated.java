9c8e8819636ead6d14e30bee85c576f42ff62f1a
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-09-19 20:56:26 -0500

    Update Truncated.java

--- Truncated.java
+++ Truncated.java
@@ -1,68 +1,37 @@
 public class Truncated implements TextBlock {
-    // +--------+------------------------------------------------------------
-    // | Fields |
-    // +--------+
-  
-    /**
-     * The original text block.
-     */
-    TextBlock originalBlock;
-  
-    /**
-     * The maximum width for truncation.
-     */
-    int maxWidth;
-  
-    // +--------------+------------------------------------------------------
-    // | Constructors |
-    // +--------------+
-  
-    /**
-     * Build a new truncated block.
-     */
+    private TextBlock contents;
+    private int maxWidth;
+
     public Truncated(TextBlock originalBlock, int maxWidth) {
-      this.originalBlock = originalBlock;
-      this.maxWidth = maxWidth;
-    } // Truncated(TextBlock, int)
-  
-    // +---------+-----------------------------------------------------------
-    // | Methods |
-    // +---------+
-  
-    /**
-     * Get one row from the block.
-     * 
-     * @pre 0 <= i < this.height()
-     * @exception Exception if the precondition is not met
-     */
+        this.contents = originalBlock;
+        this.maxWidth = maxWidth;
+    }
+
     public String row(int i) throws Exception {
-      String originalRow;
-      try {
-        originalRow = originalBlock.row(i);
-      } catch (Exception e) {
-        throw new Exception("Invalid row " + i);
-      }
-  
-      if (originalRow.length() <= maxWidth) {
-        int padding = (maxWidth - originalRow.length()) / 2;
-        String paddedContent = TBUtils.spaces(padding) + originalRow + TBUtils.spaces(padding);
-        return paddedContent;
-      } else {
-        return originalRow.substring(0, maxWidth);
-      }
-    } // row(int)
-  
-    /**
-     * Determine how many rows are in the block.
-     */
+        String originalRow;
+        try {
+            originalRow = this.contents.row(i);
+        } catch (Exception e) {
+            throw new Exception("Invalid row " + i);
+        }
+
+        if (originalRow.length() <= maxWidth) {
+            int padding = (maxWidth - originalRow.length()) / 2;
+            String paddedContent = TBUtils.spaces(padding) + originalRow + TBUtils.spaces(padding);
+            return paddedContent;
+        } else if (originalRow.length() > maxWidth ) {
+            return originalRow.substring(0, maxWidth);
+        }
+
+        return originalRow;  
+    }
+
     public int height() {
-      return originalBlock.height();
-    } // height()
-  
-    /**
-     * Determine how many columns are in the block.
-     */
+        return contents.height();
+    }
+
     public int width() {
-      return Math.min(originalBlock.width(), maxWidth);
-    } // width()
+        return Math.min(contents.width(), maxWidth);
+    }
 }
+
