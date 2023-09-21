c50d842d941471a84dbdfd85a87c85f29d07fa7d
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-09-21 13:39:45 -0500

    Update TBExpt.java

--- TBExpt.java
+++ TBExpt.java
@@ -1,6 +1,5 @@
 import java.io.PrintWriter;
 
-
 /**
  * A series of experiments with the text block layout classes.
  * 
@@ -13,92 +12,67 @@ public class TBExpt {
   // +------+
 
   public static void main(String[] args) throws Exception {
-    // Prepare for input and output
     PrintWriter pen = new PrintWriter(System.out, true);
 
-    // Create a block to use
-    TextBlock block = new TextLine("Hello");
-    TextBlock block2 = new TextLine("GoodBye");
-    TextBlock contentBlock = new TextLine("Right Justified");
-    TextBlock centerBlock = new TextLine("Centered");
+    // Truncated class
+    TextBlock truncatedText = new TextLine("Truncated");
+    BoxedBlock truncatedBox = new BoxedBlock(truncatedText);
+    TextBlock truncated = new Truncated(truncatedBox, 5);
+    TBUtils.print(pen, truncated);
 
-    TextBlock centeredBlocktest = new TextLine("Centered");
-    Centered test1 = new Centered(centeredBlocktest, 20);
-    BoxedBlock testto1 = new BoxedBlock(test1);
-
-
-    BoxedBlock box = new BoxedBlock(block);
-    BoxedBlock box2 = new BoxedBlock(box);
-    BoxedBlock box3 = new BoxedBlock(block2);
-    BoxedBlock truncatedBox = new BoxedBlock(contentBlock);
-    BoxedBlock centeredBox = new BoxedBlock(centerBlock);
-    BoxedBlock rightBlock = new BoxedBlock(contentBlock);
-    BoxedBlock hFlippedBlock = new BoxedBlock(contentBlock);
+    Truncated truncated2 = new Truncated(truncatedText, 20);
+    BoxedBlock truncatedbox2 = new BoxedBlock(truncated2);
+    TBUtils.print(pen, truncatedbox2);
 
 
-    VComposition Vbox = new VComposition(block, block2);
-    BoxedBlock vFlippedBlock = new BoxedBlock(Vbox);
-
-
-    box.row(1);
-    box2.row(0);
-    box3.row(1);
-    // Print out the block
-    /* TBUtils.print(pen, block);
-    TBUtils.print(pen, box);
-    TBUtils.print(pen, box2);
-    TBUtils.print(pen, box3); */
-
-    
-    BoxedBlock Vbox1 = new BoxedBlock(Vbox);
-    Vbox.row(1);
-    TBUtils.print(pen, Vbox1);
-    pen.println("999");
-
-
-    
-
-
-    VComposition HelloGoodbye = new VComposition(box, box3);
-    TBUtils.print(pen, HelloGoodbye);
-
-    HComposition right = new HComposition(box, block2);
-    TBUtils.print(pen, right);
-
-    HComposition left = new HComposition(block2, box);
-    TBUtils.print(pen, left);
-
-    TextBlock truncatedBlock = new Truncated(truncatedBox, 5); 
-    TBUtils.print(pen, truncatedBlock);
-
+    // Centered class
+    TextBlock centeredText = new TextLine("Centered");
+    BoxedBlock centeredBox = new BoxedBlock(centeredText);
     TextBlock centeredBlock = new Centered(centeredBox, 20);
     TBUtils.print(pen, centeredBlock);
 
-    TextBlock rightJustifiedBLock = new RightJustified(rightBlock, 20);
-    TBUtils.print(pen, rightJustifiedBLock);
+    Centered centered2 = new Centered(centeredText, 20);
+    BoxedBlock centeredBox2 = new BoxedBlock(centered2);
+    TBUtils.print(pen, centeredBox2);
 
-    TextBlock horizontallyFlippedBlock = new HorizontallyFlipped(hFlippedBlock);
+
+    // RightJustified class
+    TextBlock rightJustifiedText = new TextLine("Right Justified");
+    BoxedBlock rightJustifiedBox = new BoxedBlock(rightJustifiedText);
+    TextBlock rightJustifedBlock = new Centered(rightJustifiedBox, 20);
+    TBUtils.print(pen, rightJustifedBlock);
+
+    RightJustified rightJustified2 = new RightJustified(rightJustifiedText, 20);
+    BoxedBlock rightJustifiedBox2 = new BoxedBlock(rightJustified2);
+    TBUtils.print(pen, rightJustifiedBox2);
+
+
+    // Textline for HorizontallyFlipped, VerticallyFlipped, & HorizontalSeparator classes
+    TextBlock hello = new TextLine("Hello");
+    TextBlock goodbye = new TextLine("Goodbye");
+
+
+    // Vcomposition to create BoxedBlock
+    VComposition helloGoodbye = new VComposition(hello, goodbye);
+    BoxedBlock helloGoodbyeBox = new BoxedBlock(helloGoodbye);
+    TBUtils.print(pen, helloGoodbyeBox);
+
+
+    // HorizontallyFlipped class
+    HorizontallyFlipped horizontallyFlippedBlock = new HorizontallyFlipped(helloGoodbyeBox);
     TBUtils.print(pen, horizontallyFlippedBlock);
 
-    TextBlock verticallyFlippledBlock = new VerticallyFlipped(vFlippedBlock);
-    TBUtils.print(pen, verticallyFlippledBlock);
 
-    TextBlock horizontallySeparatedBlock = new HorizontalSeparator('#', 20);
-    BoxedBlock horizontallySeparatedBox = new BoxedBlock(horizontallySeparatedBlock);
-    TBUtils.print(pen, horizontallySeparatedBox);
+    // VerticallyFlipped class
+    VerticallyFlipped verticallyFlippedBox = new VerticallyFlipped(helloGoodbyeBox);
+    TBUtils.print(pen, verticallyFlippedBox);
 
+    
+    // HorizontalSeparator class
+    HorizontalSeparator separatedText = new HorizontalSeparator('#', 20);
+    BoxedBlock separatedTextBlock = new BoxedBlock(separatedText);
+    TBUtils.print(pen, separatedTextBlock);
 
-    TextBlock centeredtest = new Centered(centeredBlock, 20);
-        TextBlock boxedBlocktest = new BoxedBlock(centeredtest);
-        TBUtils.print(pen, testto1);
-    /* 
-    TextBlock t1 = new TextLine("Hello World");
-    TextBlock t2 = new TextLine("Hello World"); */
-
-    //boolean isEqual = TBUtils.equal(t1, t2);
-    //TBUtils.print(pen, isEqual); // This will print "Are the blocks equal? true"
-
-    // Clean up after ourselves.
     pen.close();
   } // main(String[])
 
