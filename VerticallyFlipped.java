45b5ef8bfc5a4fbaf0da5d09b6009b8da16038bb
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-11-21 21:45:17 -0600

    Update VerticallyFlipped.java

--- VerticallyFlipped.java
+++ VerticallyFlipped.java
@@ -5,27 +5,27 @@
  * September 2023
  */
 public class VerticallyFlipped implements TextBlock {
-    private TextBlock contents;
+  private TextBlock contents;
 
-    public VerticallyFlipped(TextBlock unjBox) {
-        this.contents = unjBox;
-    }
+  public VerticallyFlipped(TextBlock unjBox) {
+    this.contents = unjBox;
+  }
 
-    public String row(int i) throws Exception {
-        return this.contents.row(this.contents.height() - i - 1); // Change the positioning of each row
-    }
+  public String row(int i) throws Exception {
+    return this.contents.row(this.contents.height() - i - 1); // Change the positioning of each row
+  }
 
-    /**
-     * Determine how many rows are in the block.
-     */
-    public int height() {
-        return contents.height();
-    } // height()
+  /**
+   * Determine how many rows are in the block.
+   */
+  public int height() {
+    return contents.height();
+  } // height()
 
-    /**
-     * Determine how many columns are in the block.
-     */
-    public int width() {
-        return contents.width();
-    } // width()
+  /**
+   * Determine how many columns are in the block.
+   */
+  public int width() {
+    return contents.width();
+  } // width()
 }
