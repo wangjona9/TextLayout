29719fc811fb84bf2401b345d0ff01a5a91af465
wangjona9 <118570694+wangjona9@users.noreply.github.com>
2023-11-25 04:42:42 -0600

    Update Centered.java

--- Centered.java
+++ Centered.java
@@ -1,7 +1,8 @@
 /**
  * An implementation to center an input block
  * within a specified width, truncating if necessary.
- * Author: Jonathan Wang
+ * 
+ * @author: Jonathan Wang
  * September 2023
  */
 public class Centered implements TextBlock {
@@ -20,51 +21,57 @@ public class Centered implements TextBlock {
   // Constructors
 
   /**
-   * Build a new centered block.
+   * Constructs a new centered block.
+   *
+   * @param contents The TextBlock to be centered.
+   * @param maxWidth The maximum width for truncation.
    */
-  public Centered(TextBlock contents, int width) {
+  public Centered(TextBlock contents, int maxWidth) {
     this.contents = contents;
-    this.maxWidth = width;
+    this.maxWidth = maxWidth;
   }
 
   // Methods
 
   /**
-   * Get one row from the block.
+   * Get one row from the block, centered within the specified width.
    *
-   * @pre 0 <= i < this.height()
-   * @exception Exception if the precondition is not met
+   * @param i The index of the row.
+   * @return The centered row.
+   * @throws IllegalArgumentException if the index is out of bounds.
    */
-  public String row(int i) throws Exception {
-    String unjRow = contents.row(i);
+  public String row(int i) throws IllegalArgumentException {
+    String unjustifiedRow = contents.row(i);
 
-    // Truncate content if it's smaller than maxWidth
-    if (unjRow.length() > maxWidth) {
-      unjRow = unjRow.substring(0, maxWidth);
+    // Truncate content if it's larger than maxWidth
+    if (unjustifiedRow.length() > maxWidth) {
+      unjustifiedRow = unjustifiedRow.substring(0, maxWidth);
     }
 
-    int padding = (maxWidth - unjRow.length()) / 2;
+    int padding = (maxWidth - unjustifiedRow.length()) / 2;
 
-    // Add space to the left of the TextLine
-    String centered = String.format("%" + (padding + unjRow.length()) + "s", unjRow); 
-    
-    // Add space to the right of the TextLine
-    centered = String.format("%-" + (padding + centered.length()) + "s", centered); 
+    // Center the row within the maxWidth
+    String centeredRow = String.format("%" + (padding + unjustifiedRow.length()) + "s", unjustifiedRow);
+    centeredRow = String.format("%-" + (padding + centeredRow.length()) + "s", centeredRow);
 
-    return centered;
+    return centeredRow;
   }
 
   /**
    * Determine how many rows are in the block.
+   *
+   * @return The number of rows.
    */
   public int height() {
     return contents.height();
-  } // height()
+  }
 
   /**
    * Determine how many columns are in the block.
+   *
+   * @return The maximum width.
    */
   public int width() {
     return maxWidth;
-  } // width()
+  }
 }
